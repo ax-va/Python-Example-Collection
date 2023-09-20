446712e4dcf8983086c4cd986e4b0f38de87db9f
ax-va <vasiliev.informatik@t-online.de>
2023-09-20 22:16:47 +0200

    update

---
+++ match.py
@@ -0,0 +1,12 @@
+# Since Python 3.10
+value = "D"
+match value:
+    case "A":
+        print(value)
+    case "B":
+        print(value)
+    case "C":
+        print(value)
+    case other:
+        print(other)
+        # D

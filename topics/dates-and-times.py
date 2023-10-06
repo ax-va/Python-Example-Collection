93662ffcafa641bb7f813ae8274e03d7eb82b9b8
ax-va <vasiliev.informatik@t-online.de>
2023-10-06 20:03:21 +0200

    update

--- topics/dates-and-times.py
+++ topics/dates-and-times.py
@@ -13,5 +13,24 @@ d.isoformat()
 # '2023-10-04T09:14:27.797305'
 # Microseconds are lost.
 
-d = parser.parse('2021-11-16T22:55:48.738Z')
-# datetime.datetime(2021, 11, 16, 22, 55, 48, 738000, tzinfo=tzutc())
+# Use dateutil.parser.parse
+
+d = parser.parse('2022-01-25T22:55:44.738Z')
+# datetime.datetime(2022, 1, 25, 22, 55, 44, 738000, tzinfo=tzutc())
+
+# Use datetime.datetime.strptime
+
+d = datetime.strptime('2022-01-25 01:59:59.999', '%Y-%m-%d %H:%M:%S.%f')
+# datetime.datetime(2022, 1, 25, 1, 59, 59, 999000)
+
+d = datetime.strptime('20220125', '%Y%m%d')
+# datetime.datetime(2022, 1, 25, 0, 0)
+
+d = datetime.strptime('2022012', '%Y%m%d')
+# datetime.datetime(2022, 1, 2, 0, 0)
+
+# d = datetime.strptime('202201', '%Y%m%d')
+# # ValueError: time data '202201' does not match format '%Y%m%d'
+
+# d = datetime.strptime('2022012501', '%Y%m%d')
+# # ValueError: unconverted data remains: 01

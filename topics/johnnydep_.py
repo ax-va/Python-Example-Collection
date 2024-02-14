a95a2f140847030d1ebfd5a48e865b2d89a698a5
Alex Vasiliev <vasiliev.informatik@t-online.de>
2024-02-14 17:55:48 +0100

    update

--- topics/johnnydep_.py
+++ topics/johnnydep_.py
@@ -15,4 +15,4 @@ with open("deps.txt", "r") as file:
         deps.append((name, version))
 
 print(deps)
-"[('pandas', '2.2.0'), ('numpy', '1.26.4'), ('python-dateutil', '2.8.2'), ('pytz', '2024.1'), ('tzdata', '2024.1'), ('six', '1.16.0')]"
+# [('pandas', '2.2.0'), ('numpy', '1.26.4'), ('python-dateutil', '2.8.2'), ('pytz', '2024.1'), ('tzdata', '2024.1'), ('six', '1.16.0')]

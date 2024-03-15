6b621b24c85fee2d3b937506f9a895b6999043f9
ax-va <vasiliev.informatik@t-online.de>
2024-03-15 18:19:19 +0100

    update

---
+++ topics/decorators.py
@@ -0,0 +1,38 @@
+import functools
+from time import sleep, time
+
+
+def timing_decorator(func):
+    @functools.wraps(func)
+    def wrapper(delay):
+        print("Starting timing...")
+        start_time = time()
+        result = func(delay)
+        stop_time = time()
+        print(f"Task elapsed time: {stop_time - start_time}")
+        return result
+    return wrapper
+
+
+def complex_task1(delay):
+    sleep(delay)
+    return "Task done"
+
+
+new_complex_task = timing_decorator(complex_task1)
+print(new_complex_task(1.5))
+# Starting timing...
+# Task elapsed time: 1.5000855922698975
+# Task done
+
+
+@timing_decorator
+def complex_task2(delay):
+    sleep(delay)
+    return "task done"
+
+
+print(complex_task2(1.5))
+# Starting timing...
+# Task elapsed time: 1.5001194477081299
+# task done

da8af7342f68bb11f39d16cf3493a35d97c55667
Alex Vasiliev <vasiliev.informatik@t-online.de>
2024-07-09 10:04:25 +0200

    update

--- examples/handling_packages.py
+++ examples/handling_packages.py
@@ -156,19 +156,7 @@ if __name__ == "__main__":
         f.write("\n".join([f"{pack[0]}=={pack[1]}" for pack in pack_list_total]))
     """
 
-    requirements_filenames = [
-        r"...",
-    ]
-
-    lines = []
-    for requirements_filename in requirements_filenames:
-        with open(requirements_filename, "r") as f:
-            for line in f:
-                lines.append(line.strip() + "\n")
-
-    with open(r"...", "w") as f:
-        f.write("".join(sorted(lines, key=lambda x: re.sub(r"[#\s]+", "", x.lower()))))
-
+    """
     dep_dict = look_for_dependencies(package_list=pack_list_whl)
     # Searching for dependencies of numpy
     # Searching for dependencies of pandas
@@ -191,6 +179,21 @@ if __name__ == "__main__":
     #  ('tzdata', '2024.1'): []}
     """
 
+    """
     pack_list_total = list_packages_from_dir(from_dir=r"...", extension_split=False)
     with open(r"...", "w") as f:
         f.write("\n".join([f"{pack[0]}=={pack[1]}" for pack in pack_list_total]))
+    """
+
+    requirements_filenames = [
+        r"...",
+    ]
+
+    lines = []
+    for requirements_filename in requirements_filenames:
+        with open(requirements_filename, "r") as f:
+            for line in f:
+                lines.append(line.strip() + "\n")
+
+    with open(r"...", "w") as f:
+        f.write("".join(sorted(lines, key=lambda x: re.sub(r"[#\s]+", "", x.lower()))))

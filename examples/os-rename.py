4469a976407cd773ba64c19f5e3ebf1d6b6f444a
ax-va <vasiliev.informatik@t-online.de>
2024-12-01 18:51:24 +0100

    update

--- examples/os-rename.py
+++ examples/os-rename.py
@@ -1,5 +1,9 @@
 import os
 
-for filename in os.listdir("."):
-    if os.path.isfile(filename):
-        os.rename(filename, filename.replace("example-", ""))
+# This script should be in the same directory as the `matplotlib-examples` subdirectory
+directory = "./matplotlib-examples"
+
+for filename in os.listdir(directory):
+	filepath = os.path.join(directory, filename)
+	if os.path.isfile(filepath):
+		os.rename(filepath, filepath.replace("example-", ""))

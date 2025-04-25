97aa7bd0fbedcfe4973c875b9a3517e4fb207435
Alexander Vasiliev 🐍 💪 <85578981+ax-va@users.noreply.github.com>
2025-04-25 22:35:57 +0200

    update

--- examples/helpers/proxy/default_helper.py
+++ examples/helpers/proxy/default_helper.py
@@ -1,5 +1,5 @@
-from proxy.proxy_helper import set_default_proxy
-from proxy.ssl_certificate_helper import set_default_ssl_certificates
+from helpers.proxy.proxy_helper import set_default_proxy
+from helpers.proxy.ssl_certificate_helper import set_default_ssl_certificates
 
 
 def set_default_proxy_and_ssl_certificates() -> None:
